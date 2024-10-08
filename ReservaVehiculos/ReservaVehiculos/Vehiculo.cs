22d5455dd64ad3707ec568ffcce986c40ef77a05
carlosfaj <carlosfajardo7699@gmail.com>
2024-10-07 21:48:21 -0600

    Creamos la clase vehiculo junto con get, set y propiedades.

---
+++ ReservaVehiculos/ReservaVehiculos/Vehiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaVehiculos
+{
+    internal class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public decimal PrecioXDia { get; set; }
+    }
+}
